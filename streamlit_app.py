cff8511b3b7163a788b9c1ab40b00169dd5d4b22
FakeandrewA <143100343+FakeandrewA@users.noreply.github.com>
2024-10-25 22:04:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import numpy as np
+import matplotlib.plot as plt
 st.title('🎈 Hello world')
 
 def preprocess_img(url):
@@ -12,10 +13,8 @@ def preprocess_img(url):
 class_names = ['Tomato___Bacterial_spot', 'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold', 'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite', 'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus', 'Tomato___healthy']
 print(class_names[0])
 st.write('Hello world!')
-# Insert a chat message container.
-with st.chat_message("user"):
-    st.write("Hello 👋")
 
 # Display a chat input widget at the bottom of the app.
 user_input = st.text_input("Say something")
 st.write(user_input)
+
