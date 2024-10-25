55bd866dbe6d1cb119596abcab8ef4990d08a053
FakeandrewA <143100343+FakeandrewA@users.noreply.github.com>
2024-10-25 22:20:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 
 def preprocess_and_display_image(image_url):
-  """Downloads, preprocesses, and displays an image from a URL.
+  """Downloads, preprocesses, and displays an image from a URL in Streamlit.
 
   Args:
     image_url: The URL of the image.
@@ -20,13 +20,11 @@ def preprocess_and_display_image(image_url):
   image = tf.keras.preprocessing.image.load_img(image_path)
 
   # Preprocess the image.
-  image = tf.image.convert_image_dtype(image, dtype=tf.float32)  # Convert to float32
-  image = tf.image.resize(image, [224, 224])  # Resize to the desired size
+  image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+  image = tf.image.resize(image, [224, 224])
 
-  # Display the image.
-  plt.imshow(image)
-  plt.axis('off')  # Turn off axis labels
-  plt.show()
+  # Display the image in Streamlit.
+  st.image(image.numpy(), caption='Processed Image', use_column_width=True)
 
 class_names = ['Tomato___Bacterial_spot', 'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold', 'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite', 'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus', 'Tomato___healthy']
 print(class_names[0])
