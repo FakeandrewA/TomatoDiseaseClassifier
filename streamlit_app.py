01264c5d984c2366e2f9fe9ccc57ce2d77c10d1e
FakeandrewA <143100343+FakeandrewA@users.noreply.github.com>
2024-10-25 17:12:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,14 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+st.title('🎈 App')
+
+def preprocess_img(url):
+  # Download an image and preprocess it
+  image_path = tf.keras.utils.get_file(origin=url)
+  image = tf.keras.preprocessing.image.load_img(image_path)
+  image = tf.cast(image, dtype=tf.float32) / 255.0
+  image = tf.image.resize(image, [224, 224]).numpy()
+  return image
+
 
 st.write('Hello world!')
